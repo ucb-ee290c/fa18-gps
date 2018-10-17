24b2eded2d802a9852a65ce51d159b0c15713e79
Dinesh Parimi <dinesh.parimi@live.com>
2018-10-17 13:47:59 -0700

    parity check working, supporting encoding script added

--- python/blocks/packet_test.py
+++ python/blocks/packet_test.py
@@ -5,17 +5,19 @@ p = Packet()
 data_in = []
 for j in range(SUBFRAME_LENGTH):
     if (j == 0):
-        input_seq = [1, 0, 0, 0, 1, 0, 1, 1] + [random.randint(0, 1) for i in range(WORD_LENGTH - len(PREAMBLE))]
+        # input_seq = [1, 0, 0, 0, 1, 0, 1, 1] + [random.randint(0, 1) for i in range(WORD_LENGTH - len(PREAMBLE))]
+        input_seq = [1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0]
     else:
-        input_seq = [random.randint(0, 1) for i in range(WORD_LENGTH)]
+        # input_seq = [random.randint(0, 1) for i in range(WORD_LENGTH)]
+        input_seq = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0]
     for i in range(len(input_seq)):
         p.update(i + (j * WORD_LENGTH), input_seq[i], 0)
         data_in.append(input_seq[i])
 p.update(WORD_LENGTH * SUBFRAME_LENGTH, 0, 0)
 
 flatten = lambda l: [item for sublist in l for item in sublist]
-out_flat = flatten(p.out)
-if (flatten(p.out) == data_in):
+out_flat = flatten(p.parser.out)
+if (flatten(p.parser.out) == data_in):
     print("Test successful.")
 else:
-    out_flat = flatten(p.out)
+    print("Test failed.")
