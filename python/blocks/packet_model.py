8304cbf7bf41e53d114b9ce457ea772492334f6c
Dinesh Parimi <dinesh.parimi@live.com>
2018-10-17 09:26:19 -0700

    working packetizer but no parity check yet

--- python/blocks/packet_model.py
+++ python/blocks/packet_model.py
@@ -5,6 +5,8 @@ from enum import Enum
 from block import Block
 
 PREAMBLE = [1, 0, 0, 0, 1, 0, 1, 1]
+WORD_LENGTH = 30
+SUBFRAME_LENGTH = 10
 
 class State(Enum):
     WAITING = 0
@@ -14,19 +16,42 @@ class State(Enum):
 #TODO: Finish Packetizer class
 class Packet(Block):
     def __init__(self):
-        self.data = [0] * 8
+        self.fifo = [0] * 8
         self.state = State.WAITING
+        self.data = []
+        self.word = []
+        self.word_idx = 0
+        self.bit_idx = 0
+        self.out = []
+        self.valid = False
 
     def update(self, cycle, I_int, Q_int):
-        self.data = self.data[1:] + [I_int]
-        print(str(cycle) + ": Packetizer FIFO contains " + ''.join(map(str, self.data)))
+        self.fifo = self.fifo[1:] + [I_int]
+        print(str(cycle) + ": Packetizer FIFO contains " + ''.join(map(str, self.fifo)))
         if self.state == State.WAITING:
-            if self.data == PREAMBLE:
+            if self.fifo == PREAMBLE:
                 print("Preamble detected!")
                 self.state = State.PARSING
+                self.word[:] = self.fifo[:]
+                self.bit_idx = 8
+                self.word_idx = 0
         elif self.state == State.PARSING:
-            pass
+            self.word.append(I_int)
+            self.bit_idx += 1
+            if self.bit_idx == WORD_LENGTH:
+                self.data.append(self.word[:])
+                self.word[:] = []
+                self.bit_idx = 0
+                self.word_idx += 1
+                if self.word_idx == SUBFRAME_LENGTH:
+                    self.state = State.DONE
+                    return
         elif self.state == State.DONE:
+            print("Writing data to memory map.")
+            self.out[:] = self.data[:]
+            self.data[:] = []
+            self.state = State.WAITING
             pass
         else:
             raise Exception("Invalid state detected in packetizer.")
+        self.valid = (self.state == State.DONE)
---
+++ python/blocks/packet_test.py
@@ -0,0 +1,18 @@
+from packet_model import *
+import random
+
+p = Packet()
+data_in = []
+for j in range(SUBFRAME_LENGTH):
+    if (j == 0):
+        input_seq = [1, 0, 0, 0, 1, 0, 1, 1] + [random.randint(0, 1) for i in range(WORD_LENGTH - len(PREAMBLE))]
+    else:
+        input_seq = [random.randint(0, 1) for i in range(WORD_LENGTH)]
+    for i in range(len(input_seq)):
+        p.update(i + (j * WORD_LENGTH), input_seq[i], 0)
+        data_in.append(input_seq[i])
+p.update(WORD_LENGTH * SUBFRAME_LENGTH, 0, 0)
+
+flatten = lambda l: [item for sublist in l for item in sublist]
+if (flatten(p.out) == data_in):
+    print("Test successful.")
