12f2b726211e97debd263ab80a608e359a4fac0b
Dinesh Parimi <dinesh.parimi@live.com>
2018-10-16 22:59:06 -0700

    added packetizer fifo to python model

--- python/blocks/packet_model.py
+++ python/blocks/packet_model.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from .block import Block
+from block import Block
 
 #TODO: Finish Packetizer class
 class Packet(Block):
+    def __init__(self):
+        self.data = [0] * 8
+
     def update(self, cycle, I_int, Q_int):
-        print(str(cycle) + ": Packetized data should be printed here")
-        return 0
-
-
+        self.data = self.data[1:] + [I_int]
+        print(str(cycle) + ": Packetizer FIFO contains " + ''.join(map(str, self.data)))
