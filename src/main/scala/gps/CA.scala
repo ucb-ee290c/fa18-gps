2f54da239e020d3925d67be098111e1b17329eea
nwerblun <nickwerblun@gmail.com>
2018-10-20 21:53:43 -0700

    In theory it should work

--- src/main/scala/gps/CA.scala
+++ src/main/scala/gps/CA.scala
@@ -5,17 +5,35 @@ import chisel3.experimental.FixedPoint
 import chisel3.util._
 import dsptools.numbers._
 
-case class CAParams (
+is(class CAParams (
   val fcoWidth: Int,
   val codeWidth: Int
 )
 
+class CACodeShiftReg(params: CAParams) extends Module {
+  val io = IO(new Bundle {
+    val codeIn = Input(SInt(params.codeWidth.W))
+    val fco2x = Input(SInt(params.fcoWidth.W))
+    val punctual = Output(SInt(params.codeWidth.W))
+    val late = Output(SInt(params.codeWidth.W))
+  })
+  val punctual = RegInit(1.S(params.codeWidth.W))
+  val late = RegInit(1.S(params.codeWidth.W))
+  val prev_tick = RegInit(0.S(params.fcoWidth.W))
+  prev_tick := io.fco2x
+  when(prev_tick <= 0.S && io.fco2x > 0.S) {
+    punctual := io.codeIn
+    late := punctual
+  }
+  io.punctual := punctual
+  io.late := late
+}
 
 class CA(params: CAParams) extends Module {
     val io = IO(new Bundle {
         val satellite = Input(UInt(6.W)) //There are 32 possible feedbacks. Need 6 bits
         val fco = Input(SInt(params.fcoWidth.W))
-        val fco_2x = Input(SInt(params.fcoWidth.W))
+        val fco2x = Input(SInt(params.fcoWidth.W))
 
         val early = Output(SInt(params.codeWidth.W)) //
         val punctual = Output(SInt(params.codeWidth.W))
@@ -26,55 +44,54 @@ class CA(params: CAParams) extends Module {
     val feedbackPos = RegInit(VecInit(Seq.fill(2)(0.U(params.codeWidth.W))))
     switch(io.satellite) {
      is(1.U){ feedbackPos := Seq(2.U,6.U) }
-     /*
      is(2.U){ feedbackPos := Seq(3.U,7.U) }
      is(3.U){ feedbackPos := Seq(4.U,8.U) }
      is(4.U){ feedbackPos := Seq(5.U,9.U) }
-     */
-     /*
-     case 5.U => Seq(1.U,9.U)
-     case 6.U => Seq(2.U,10.U)
-     case 7.U => Seq(1.U,8.U)
-     case 8.U => Seq(2.U,9.U)
-     case 9.U => Seq(3.U,10.U)
-     case 10.U => Seq(2.U,3.U)
-     case 11.U => Seq(3.U,4.U)
-     case 12.U => Seq(5.U,6.U)
-     case 13.U => Seq(6.U,7.U)
-     case 14.U => Seq(7.U,8.U)
-     case 15.U => Seq(8.U,9.U)
-     case 16.U => Seq(9.U,10.U)
-     case 17.U => Seq(1.U,4.U)
-     case 18.U => Seq(2.U,5.U)
-     case 19.U => Seq(3.U,6.U)
-     case 20.U => Seq(4.U,7.U)
-     case 21.U => Seq(5.U,8.U)
-     case 22.U => Seq(6.U,9.U)
-     case 23.U => Seq(1.U,3.U)
-     case 24.U => Seq(4.U,6.U)
-     case 25.U => Seq(5.U,7.U)
-     case 26.U => Seq(6.U,8.U)
-     case 27.U => Seq(7.U,9.U)
-     case 28.U => Seq(8.U,10.U)
-     case 29.U => Seq(1.U,6.U)
-     case 30.U => Seq(2.U,7.U)
-     case 31.U => Seq(3.U,8.U)
-     case 32.U => Seq(4.U,9.U)
+     is(5.U){ feedbackPos := Seq(1.U,9.U) }
+     is(6.U){ feedbackPos :=  Seq(2.U,10.U) }
+     is(7.U){ feedbackPos :=  Seq(1.U,8.U) }
+     is(8.U){ feedbackPos :=  Seq(2.U,9.U) }
+     is(9.U){ feedbackPos :=  Seq(3.U,10.U) }
+     is(10.U){ feedbackPos :=  Seq(2.U,3.U) }
+     is(11.U){ feedbackPos :=  Seq(3.U,4.U) }
+     is(12.U){ feedbackPos :=  Seq(5.U,6.U) }
+     is(13.U){ feedbackPos := Seq(6.U,7.U) }
+     is(14.U){ feedbackPos := Seq(7.U,8.U) }
+     is(15.U){ feedbackPos := Seq(8.U,9.U) }
+     is(16.U){ feedbackPos := Seq(9.U,10.U) }
+     is(17.U){ feedbackPos := Seq(1.U,4.U) }
+     is(18.U){ feedbackPos := Seq(2.U,5.U) }
+     is(19.U){ feedbackPos := Seq(3.U,6.U) }
+     is(20.U){ feedbackPos := Seq(4.U,7.U) }
+     is(21.U){ feedbackPos := Seq(5.U,8.U) }
+     is(22.U){ feedbackPos := Seq(6.U,9.U) }
+     is(23.U){ feedbackPos := Seq(1.U,3.U) }
+     is(24.U){ feedbackPos := Seq(4.U,6.U) }
+     is(25.U){ feedbackPos := Seq(5.U,7.U) }
+     is(26.U){ feedbackPos := Seq(6.U,8.U) }
+     is(27.U){ feedbackPos := Seq(7.U,9.U) }
+     is(28.U){ feedbackPos := Seq(8.U,10.U) }
+     is(29.U){ feedbackPos := Seq(1.U,6.U) }
+     is(30.U){ feedbackPos := Seq(2.U,7.U) }
+     is(31.U){ feedbackPos := Seq(3.U,8.U) }
+     is(32.U){ feedbackPos := Seq(4.U,9.U) }
      */
     }
-    
+    val prev_tick = RegInit(0.S(params.fcoWidth.W))
     val curr_sv = RegInit(0.U(6.W)) //32 sattelites, can hardcode this width
     val counter = RegInit(0.U(log2Ceil(1024).W)) //Code length always 1023, can hardcode width
     val g1FeedbackReg = RegInit(0.S(params.codeWidth.W)) 
     val g2FeedbackReg = RegInit(0.S(params.codeWidth.W)) 
     val g1 = RegInit(VecInit(Seq.fill(10)(1.S(params.codeWidth.W))))
     val g2 = RegInit(VecInit(Seq.fill(10)(1.S(params.codeWidth.W))))
+    prev_tick := io.fco
     //Want to restart the sequence if the satellite changes or if we complete one full sequence 
     when((curr_sv =/= io.satellite) || (counter === 1023.U)) {
         curr_sv := io.satellite
-        g1 := RegInit(VecInit(Seq.fill(10)(1.S(params.codeWidth.W))))
-        g2 := RegInit(VecInit(Seq.fill(10)(1.S(params.codeWidth.W))))
-    }.otherwise {
+        prev_tick := 0.S(params.fcoWidth.W)
+        g1 := VecInit(Seq.fill(10)(1.S(params.codeWidth.W)))
+        g2 := VecInit(Seq.fill(10)(1.S(params.codeWidth.W)))
+    }.elsewhen(prev_tick <= 0.S && io.fco > 0.S) {
       //Feedback to the first element in the shift register by adding mod 2 
       g1FeedbackReg := (g1(2.U) + g1(9.U)) % 2.S //feedback is always 3 and 9, but that's for 1 indexing
       g2FeedbackReg := (g2(1.U) + g2(2.U) + g2(5.U) + g2(7.U) + g2(8.U) + g2(9.U)) % 2.S
@@ -87,11 +104,14 @@ class CA(params: CAParams) extends Module {
       g1(0.U) := g1FeedbackReg
       g2(0.U) := g2FeedbackReg
     }
+    val shifts = Module(new CACodeShiftReg(params))
     //Feedback for g1 is always from position 10 (off by 1)
     //Feedback for g2 is an xor of 2 positions based on the sattelite
     val res = (g1(9.U) + (g2(feedbackPos(0.U)) + g2(feedbackPos(1.U)))) % 2.S
     io.early := Mux(res === 1.S, 1.S(params.codeWidth.W), -1.S(params.codeWidth.W)) 
-    io.punctual := 0.S(params.codeWidth.W)
-    io.late := 0.S(params.codeWidth.W)
     io.done := counter === 1023.U
+    shifts.io.fco2x := io.fco2x
+    shifts.io.codeIn := io.early
+    io.punctual := shifts.io.punctual
+    io.late := shifts.io.late
 }
