4cec96e239669b43f6bb9a84aee9c4fea5b79b63
nwerblun <nickwerblun@gmail.com>
2018-10-21 21:24:57 -0700

    Significant progress made in debugging. Tests are a WIP while debugging continues.

---
+++ src/test/scala/CASpec.scala
@@ -0,0 +1,41 @@
+package gps
+
+import dsptools.numbers._
+import org.scalatest.{FlatSpec, Matchers}
+import scala.io._
+class CASpec extends FlatSpec with Matchers {
+  behavior of "CA"
+  val params = CAParams(
+    fcoWidth = 10,
+    codeWidth = 2,
+  )
+  it should "give the correct early PRN" in {
+      val prnCodeRaw = io.Source.fromFile("/home/nwerblun/ee290c/fa18-gps/src/test/scala/PRNCode.csv").getLines.toList.map(_.split(","))
+  //val prnCode = prnCodeRaw(0).map(_.toInt)
+  //Creates an array of 32 arrays each of which has a 1023 length PRN sequence
+  val prnCodes = Array.ofDim[Int](prnCodeRaw.length, prnCodeRaw(0).length)
+  for(i <- 0 until prnCodeRaw.length) {
+    prnCodes(i) = prnCodeRaw(i).map(_.toInt)
+  }
+  val ncoInput = new Array[Int](1023)
+  for(i <- 0 until ncoInput.length) {
+    if (i % 2 == 0) { ncoInput(i) = -1 }
+    else { ncoInput(i) = 1 }
+  }
+  //Tests if the early signal is what's expected.
+    CAEarlyTester(params, prnCodes, ncoInput) should be (true)
+  }
+  /*
+  //Tests if there's never a zero crossing on the NCO that the output is always the same
+  it should "never change" in {
+    val trials_2 = 
+    CANoChangeTester(params, trials_2) should be (true)
+  }
+  //tests what happens if you switch the satellite midway through 
+  it should "correctly reset" in {
+   val trials_3 = 
+   CASatelliteSwitchTester(params, trials_3) should be (true)
+   }
+  */
+}
+
