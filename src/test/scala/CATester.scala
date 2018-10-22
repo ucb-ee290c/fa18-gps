585865ccd72f68e1447c86a05e320b82c3970ef8
nwerblun <nickwerblun@gmail.com>
2018-10-22 11:44:17 -0700

    Added a test that verifies nothing happens when there's no 0 crossings. It passes. Woo

--- src/test/scala/CATester.scala
+++ src/test/scala/CATester.scala
@@ -14,9 +14,9 @@ class CAEarlyTester(c: CA, prnCodes: Array[Array[Int]], ncoInput: Array[Int]) ex
   the CA can properly output the early signal. This test does not determine if punctual/late are
   correct. This test also verifies that the done signal properly goes high when a cycle is complete.
    */
-  //val outputs = new Array[Int](1023)
   poke(c.io.fco2x, 0)
   for(j <- 0 until 32) {
+    //The CA has 1 indexed satellite feedback positions, so we need 1 - 32 not 0 - 31
     poke(c.io.satellite, j + 1)
     for(i <- 0 until ncoInput.length) {
       poke(c.io.fco, ncoInput(i))
@@ -25,8 +25,8 @@ class CAEarlyTester(c: CA, prnCodes: Array[Array[Int]], ncoInput: Array[Int]) ex
       step(1)
       //NCO test is -1, 1, -1, 1 so it takes 2 cycles to actually update. Hence the i % 2
       //We're updating 2x as fast as prnCodes changes so i/2
+      //The PRN Codes list is 0 indexed, so j is fine
       if (i % 2 == 0) { expect(c.io.early, 2*prnCodes(j)(i/2) - 1) }
-      peek(c.io.counter)
     }
     expect(c.io.done, 1)
   }
@@ -42,4 +42,33 @@ object CAEarlyTester {
   }
 }
 
+
+class CANoOutputTester(c: CA, prnCodes: Array[Array[Int]], ncoInput: Array[Int]) extends DspTester(c) {
+  /*
+    This test checks that if there's never a 0 crossing (constant input) that the output will not
+    never change. Done should also never go high since the sequence is never completed.
+  */
+  //All 3 outputs should be the first entry forever for each satellite. 
+  poke(c.io.fco2x, 0)
+  for(j <- 0 until 32) {
+    poke(c.io.satellite, j + 1)
+    for(i <- 0 until ncoInput.length) {
+      poke(c.io.fco, ncoInput(i))
+      expect(c.io.done, 0)
+      step(1)
+      //See above test for j vs satellite -> j+1
+      expect(c.io.early, prnCodes(j)(0))
+      expect(c.io.punctual, prnCodes(j)(0))
+      expect(c.io.late, prnCodes(j)(0))
+    }
+    expect(c.io.done, 0)
+  }
+}
+object CANoOutputTester {
+  def apply(params: CAParams, prnCodes: Array[Array[Int]], ncoInput: Array[Int]): Boolean = {
+    chisel3.iotesters.Driver.execute(Array("-tbn", "firrtl", "-fiwv"), () => new CA(params)) {
+      c => new CANoOutputTester(c, prnCodes, ncoInput)
+    }
+  }
+}
 
