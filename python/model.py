bee6042dcc173bf1ba3cbb7eb4155d843a26af74
Avi <aviral0607@berkeley.edu>
2018-10-24 00:12:39 -0700

    Gotten model.py working

--- python/model.py
+++ python/model.py
@@ -20,19 +20,23 @@ sv = sv_list[0]
 sv_freq = sv_freqs[0]
 
 # We'll start off with the NCO Width set to 10
-nco_width = 10
+nco_width = 18
 ideal_carrier_nco_code = sv_freq*1e6 * (2**nco_width) / fs
 print(f"""Ideal NCO frequency for SV {sv} with frequency {sv_freq} is
 {ideal_carrier_nco_code}""")
 carrier_nco_code = round(ideal_carrier_nco_code)
 print(f"Rounding the NCO frequency to {carrier_nco_code}")
 
+#Set the integration period
+integration_period = 5
+
+
 def main():
     num_cycles = len(raw_data)//10
-    print(num_cycles)
+    print(f' Simulating for {num_cycles}')
 
     adc = ADC(np.sign(raw_data))
-    nco_carrier = NCO(10, False)
+    nco_carrier = NCO(nco_width, False)
 
     # Technically don't need to make multiple multiplier objects as they all
     # behave the same. But in the code we are creating multiple object
@@ -53,34 +57,33 @@ def main():
     intdumpQ = IntDump()
 
     # ki = 1, kp = 1, second discriminator
-    dll = DLL(1, 1, 1)
+    dll = DLL(2, 1, 1)
     # Disabling the Costas loop for now and forcing the right frequency
     costas = Costas(1,[1,1,1],1,1,1)    
 
-    nco_code = NCO(10, True)
+    nco_code = NCO(nco_width, True)
     packet = Packet()
-    
-    # FIXME: Initial DLL and Costas loop values
+
+    # Acquisition is not in the loop yet, we'll correctly set these for now
     dll_out = 1023e3*(2**nco_width)/fs
     costas_out = carrier_nco_code
-    last_integ_I = [0,0,0]
-    last_integ_Q = [0,0,0]
+   
+    # Instrumentation: 
     dll_out_arr = []
-    processed = False
-    dll_I_e = []
-    dll_I_p = []
-    dll_I_l = []
-    dis_out_arr = []
+    e_mags = []
+    p_mags = []
+    l_mags = []
 
-    for x in range(0, num_cycles):
+    integration_count = 1
+
+    for x in range(num_cycles):
         adc_data = adc.update()
         cos_out, sin_out  = nco_carrier.update(costas_out)        
         I = multI.update(adc_data, cos_out)
         Q = multQ.update(adc_data, sin_out)
 
         f_out, f2_out = nco_code.update(dll_out)
-        e, p, l, dump = ca.update(f_out, f2_out, sv)
-        # print(e, p, l, f_out, f2_out)
+        e, p, l, done = ca.update(f_out, f2_out, sv)
 
         I_e = multIe.update(I, e)        
         I_p = multIp.update(I, p)
@@ -92,37 +95,37 @@ def main():
         I_sample = [I_e, I_p, I_l]
         Q_sample = [Q_e, Q_p, Q_l]
 
+        if done:
+            integration_count += 1
+
         # We'll do this calc here, since intdump throws things away when dump=1
-        if dump and not processed:
-            print(x)
+        if integration_count == integration_period:
+            e_mags.append(np.sqrt(I_int[0]**2 + Q_int[0]**2))
+            p_mags.append(np.sqrt(I_int[1]**2 + Q_int[1]**2))
+            l_mags.append(np.sqrt(I_int[2]**2 + Q_int[2]**2))
+            print(f'Time: {x}, e: {e_mags[-1]}, p: {p_mags[-1]}, l: {l_mags[-1]}')
             dll_out, dis_out = dll.update(I_int, Q_int, 1023e3*(2**nco_width)/fs, 0)
-            print(f"Early: {I_int[0]}, Late: {I_int[2]}, Out: {dll_out}")
-            dll_I_e.append(I_int[0])
-            dll_I_p.append(I_int[1])
-            dll_I_l.append(I_int[2])
-            dll_out = np.round(dll_out)
             dll_out_arr.append(dll_out)
-            processed = True
-        elif not dump: 
-            processed = False
+            integration_count = 0
         
         # Commenting this out for now
         # costas_out = costas.update(I_int[1], I_int[1], 0)
         
         # I_int and Q_int are lists of size 3
-        I_int, reset = intdumpI.update(I_sample, dump)
-        Q_int, reset = intdumpQ.update(Q_sample, dump)
-
+        I_int, reset = intdumpI.update(I_sample, 
+            integration_count == integration_period)
+        Q_int, reset = intdumpQ.update(Q_sample,
+            integration_count == integration_period)
+ 
         # packet.update(x, I_int, Q_int)
 
     plt.figure()
     plt.plot(dll_out_arr)
     plt.figure()
-    plt.plot(dll_I_e, label='early')
-    plt.plot(dll_I_p, label='prompt')
-    plt.plot(dll_I_l, label='late')
+    plt.plot(e_mags, label='early')
+    plt.plot(p_mags, label='prompt')
+    plt.plot(l_mags, label='late')
     plt.legend()
-    
     
     plt.show()
 
