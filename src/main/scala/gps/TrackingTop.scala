1eb1d938304498443fa1c97a05e507872e7924d7
dpgrubb13 <dpgrubb@berkeley.edu>
2018-11-01 01:04:51 -0700

    additions to TrackingTop; still need more modules to hook up

--- src/main/scala/gps/TrackingTop.scala
+++ src/main/scala/gps/TrackingTop.scala
@@ -12,6 +12,7 @@ trait TrackingTopParams {
 class TrackingTopIO(params: TrackingTopParams) extends Bundle {
   val adcSample = Input(SInt(params.adcWidth.W))
   val svNumber = Input(UInt(6.W)) //fixed width due to number of satellites
+  //also needs bias for tracking loops
 }
 
 object TrackingTopIO {
@@ -23,73 +24,86 @@ object TrackingTopIO {
 class TrackingTop (val params: TrackingTopParams) extends Module {
   val io = IO(TrackingTopIO(params))
 
-//  val CAGen = Module(new CA(CAParams(fcoWidth, codeWidth)))
-//  CAGen.io.satellite := io.svNumber
-//  CAGen.io.fco := fromNCO
-//  CAGen.io.fco2x := from NCO2
+  val costasLoop = Module(new Costas[SInt](SampledCostasParams(dataWidth=4*params.adcWidth, freqWidth=4*params.adcWidth, phaseWidth=4*params.adcWidth)))
+  //  costasLoop.io.freqBias := //freqBias from acquisition
+
+  val codeNCO = Module(new NCO[SInt](FixedNcoParams(width=32, sinOut=false)))
+//  codeNCO.io.stepSize := //TODO: from DLL
+
+  val carrierNCO = Module(new NCO[SInt](FixedNcoParams(width=32, sinOut=true)))
+  carrierNCO.io.stepSize := costasLoop.io.freqCtrl
+  //TODO: add phase control
+
+  val CAGen = Module(new CA(CAParams(fcoWidth = 5, codeWidth = 6)))
+  CAGen.io.satellite := io.svNumber
+  CAGen.io.fco := codeNCO.io.cos
+  CAGen.io.fco2x := codeNCO.io.cos //TODO: get the 2x frequency
 
   //instantiate DLL, Costas Loop, NCO's, packetizer etc. //TODO: Instantiate remaining blocks
+
+//  val packetizer = Module(new Packetizer(PacketizerParams()))
 
   //could make into a vec if wanted it to be more variable length and connections more concise
   val multI = Module(new Mul[SInt](SampledMulParams(params.adcWidth)))
   multI.io.in1 := io.adcSample
-//  multI.io.in2 := costasLoopOut.cos
+  multI.io.in2 := carrierNCO.io.cos
 
   val multQ = Module(new Mul[SInt](SampledMulParams(params.adcWidth)))
   multQ.io.in1 := io.adcSample
-//  multQ.io.in2 := costasLoopOut.sin
+  multQ.io.in2 := carrierNCO.io.sin
 
   val multIE = Module(new Mul[SInt](SampledMulParams(2*params.adcWidth)))
   multIE.io.in1 := multI.io.out
-//  multIE.io.in2 := CAGen.io.early
+  multIE.io.in2 := CAGen.io.early
 
   val multIP = Module(new Mul[SInt](SampledMulParams(2*params.adcWidth)))
   multIP.io.in1 := multI.io.out
-//  multIP.io.in2 := CAGen.io.punctual
+  multIP.io.in2 := CAGen.io.punctual
 
   val multIL = Module(new Mul[SInt](SampledMulParams(2*params.adcWidth)))
   multIL.io.in1 := multI.io.out
-//  multIL.io.in2 := CAGen.io.late
+  multIL.io.in2 := CAGen.io.late
 
   val multQE = Module(new Mul[SInt](SampledMulParams(2*params.adcWidth)))
   multQE.io.in1 := multQ.io.out
-//  multQE.io.in2 := CAGen.io.early
+  multQE.io.in2 := CAGen.io.early
 
   val multQP = Module(new Mul[SInt](SampledMulParams(2*params.adcWidth)))
   multQP.io.in1 := multQ.io.out
-//  multQP.io.in2 := CAGen.io.punctual
+  multQP.io.in2 := CAGen.io.punctual
 
   val multQL = Module(new Mul[SInt](SampledMulParams(2*params.adcWidth)))
   multQL.io.in1 := multQ.io.out
-//  multQL.io.in2 := CAGen.io.late
+  multQL.io.in2 := CAGen.io.late
 
 
   //again, could make into a vec? might make naming less clear
-  val intDumpIE = Module(new IntDump[SInt](SampledIntDumpParams(params.adcWidth, params.codeLength))) //TODO: change bitwidth here to proper value (output of Mul or truncated)
+  val intDumpIE = Module(new IntDump[SInt](SampledIntDumpParams(4*params.adcWidth, params.codeLength)))
   intDumpIE.io.in := multIE.io.out
 //  intDumpIE.io.dump := dump
 
-  val intDumpIP = Module(new IntDump[SInt](SampledIntDumpParams(params.adcWidth, params.codeLength)))
+  val intDumpIP = Module(new IntDump[SInt](SampledIntDumpParams(4*params.adcWidth, params.codeLength)))
   intDumpIP.io.in := multIP.io.out
 //  intDumpIP.io.dump := dump
 
-  val intDumpIL = Module(new IntDump[SInt](SampledIntDumpParams(params.adcWidth, params.codeLength)))
+  val intDumpIL = Module(new IntDump[SInt](SampledIntDumpParams(4*params.adcWidth, params.codeLength)))
   intDumpIL.io.in := multIL.io.out
 //  intDumpIL.io.dump := dump
 
-  val intDumpQE = Module(new IntDump[SInt](SampledIntDumpParams(params.adcWidth, params.codeLength)))
+  val intDumpQE = Module(new IntDump[SInt](SampledIntDumpParams(4*params.adcWidth, params.codeLength)))
   intDumpQE.io.in := multQE.io.out
 //  intDumpQE.io.dump := dump
 
-  val intDumpQP = Module(new IntDump[SInt](SampledIntDumpParams(params.adcWidth, params.codeLength)))
+  val intDumpQP = Module(new IntDump[SInt](SampledIntDumpParams(4*params.adcWidth, params.codeLength)))
   intDumpQP.io.in := multQP.io.out
 //  intDumpQP.io.dump := dump
 
-  val intDumpQL = Module(new IntDump[SInt](SampledIntDumpParams(params.adcWidth, params.codeLength)))
+  val intDumpQL = Module(new IntDump[SInt](SampledIntDumpParams(4*params.adcWidth, params.codeLength)))
   intDumpQL.io.in := multQL.io.out
 //  intDumpQL.io.dump := dump
 
-
+  costasLoop.io.Ip := intDumpIP.io.integ
+  costasLoop.io.Qp := intDumpQP.io.integ
 
 
 }
