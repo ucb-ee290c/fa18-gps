6dcba3fcac50877684409460032a51248a501eb6
zhaokai-l <zhaokai_liu@berkeley.edu>
2018-11-05 10:17:38 -0800

    add tester for FFT mulitplication blocks, not finish

--- src/main/scala/gps/FFT/FFTMul.scala
+++ src/main/scala/gps/FFT/FFTMul.scala
@@ -1,5 +1,6 @@
 package gps
 
+import breeze.numerics.{log, log10}
 import chisel3._
 import chisel3.util.RegEnable
 import chisel3.experimental.FixedPoint
@@ -9,74 +10,55 @@ import dsptools.numbers._
 
 import scala.collection._
 
-/**
-  * Base class for FIR Filter parameters
-  *
-  * These are type generic
-  */
-trait FIRFilterParams[T <: Data] {
+trait FFTMulParams[T <: Data] {
   val protoData: T
-  val taps: Seq[T]
-  //TODO: Implementation -> Direct or Transposed?
-  //TODO: # of Pipeline Stages?
-  //TODO: Data Buffer Type -> Linear Shift Register or Circular Buffer?
+  val lanes: Int // How many inputs
+  val pipeStages: Int // How many pipelined output
 }
 
-case class FixedFIRFilterParams(
-                                 width: Int,
-                                 bp: Int,
-                                 tapSeq: Seq[FixedPoint]
-) extends FIRFilterParams[FixedPoint] {
+case class FixedFFTMulParams(
+  width: Int,
+  bp: Int,
+  laneCount: Int,
+  pipeStageCount: Int,
+) extends FFTMulParams[FixedPoint] {
   val protoData = FixedPoint(width.W, bp.BP)
-  val taps = tapSeq
+  val lanes = laneCount
+  val pipeStages = pipeStageCount
 }
 
 /**
   * Bundle type as IO for FIR Filter modules
   */
-class FIRFilterIO[T <: chisel3.Data : Ring](params: FIRFilterParams[T]) extends Bundle {
-  val in = Flipped(ValidWithSync(params.protoData.cloneType))
-  val out = ValidWithSync(params.protoData.cloneType)
-
-  override def cloneType: this.type = FIRFilterIO(params).asInstanceOf[this.type]
+class FFTMulIO[T <: chisel3.Data : Ring](params: FFTMulParams[T]) extends Bundle {
+  val dataIn = Input(ValidWithSync(Vec(params.lanes, params.protoData.cloneType)))
+  val caIn = Input(ValidWithSync(Vec(params.lanes, params.protoData.cloneType)))
+  val out = Output(ValidWithSync(Vec(params.lanes, params.protoData.cloneType)))
+  //TODO: does it need extend bits?
+  override def cloneType: this.type = FFTMulIO(params).asInstanceOf[this.type]
 }
-object FIRFilterIO {
-  def apply[T <: chisel3.Data : Ring](params: FIRFilterParams[T]): FIRFilterIO[T] =
-    new FIRFilterIO(params)
+object FFTMulIO {
+  def apply[T <: chisel3.Data : Ring](params: FFTMulParams[T]): FFTMulIO[T] =
+    new FFTMulIO(params)
 }
 
-class ConstantCoefficientFIRFilter[T <: chisel3.Data : Ring](val params: FIRFilterParams[T]) extends Module {
-  require(params.taps.nonEmpty)
-  val io = IO(FIRFilterIO[T](params))
-
+class FFTMul[T <: chisel3.Data : Ring](val params: FFTMulParams[T]) extends Module {
+  require(params.lanes > 0, "Must have parallel input size greater than 1")
+  require(params.pipeStages > 0, "pipeline stage numbers must greater than 1, should depends on FFT pipeline depth")
+  val io = IO(FFTMulIO[T](params))
   val shift_en = Wire(Bool())
+  val counter = RegInit(UInt( ((log10(params.lanes)/log10(2)).ceil.toInt+1).W ),0.U)
 
-  val regs = mutable.ArrayBuffer[T]()
-  for(i <- params.taps.indices) {
-    if(i == 0) regs += RegEnable(io.in.bits, Ring[T].zero, shift_en)
-    else       regs += RegEnable(regs(i - 1), Ring[T].zero, shift_en)
-  }
 
-  val muls = mutable.ArrayBuffer[T]()
-  for(i <- params.taps.indices) {
-    muls += regs(i) * params.taps(i)
-  }
-
-  val accumulator = mutable.ArrayBuffer[T]()
-  for(i <- params.taps.indices) {
-    if (i == 0) accumulator += muls(i)
-    else accumulator += muls(i) + accumulator(i - 1)
-  }
-
-  when(io.in.valid === true.B) {
+  when(io.dataIn.valid === true.B && io.caIn.valid === true.B) {
     shift_en := true.B
+    io.out.bits.zip(io.dataIn.bits.zip(io.caIn.bits)).foreach{case (o,(data, ca)) => o := data * ca}
+    io.out.valid := true.B
   } .otherwise{
     shift_en := false.B
+    io.out.valid := false.B
   }
 
-  io.out.bits := accumulator.last
-  io.out.valid := (ShiftRegisterWithReset(io.in.valid, params.taps.length, false.B, shift_en) && shift_en)
-  io.out.sync := (ShiftRegisterWithReset(io.in.sync, params.taps.length, false.B, shift_en) && shift_en)
+  io.out.sync := (ShiftRegisterWithReset(io.dataIn.valid, params.pipeStages, false.B, shift_en) && shift_en)
 
 }
-
