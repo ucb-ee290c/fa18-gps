642c2c75e2509d73d18ca7c95b420c3567affe0c
nwerblun <nickwerblun@gmail.com>
2018-11-25 22:31:05 -0800

    Fixed sat. pos. calculations. Added test to verify correct behavior.

--- firmware/calculations.h
+++ firmware/calculations.h
@@ -4,6 +4,7 @@
 # include <stdlib.h>
 # include "params.h"
 
-double t_k(float t, struct rcv_params *params);
+float t_k(float t, struct rcv_params *params);
 float eccentric_anomaly(float Mk, struct rcv_params *params);
 void get_sv_pos(float t, float *Xx, float *Yy, float *Zz, float *xpl, float *ypl, struct rcv_params *params);
+#endif
