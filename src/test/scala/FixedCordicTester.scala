5d3a4a9b5bb4bb62a3623fdbe9ef349ad6b321c4
Avi <aviral0607@berkeley.edu>
2018-12-13 15:17:24 -0800

    some work on updating tester

--- src/test/scala/FixedCordicTester.scala
+++ src/test/scala/FixedCordicTester.scala
@@ -1,6 +1,7 @@
 package gps
 
 import dsptools.DspTester
+import org.scalatest.{FlatSpec, Matchers}
 
 case class ABC(
                 xin: Double,
@@ -44,5 +45,65 @@ object FixedCordicTester {
     chisel3.iotesters.Driver.execute(Array("-fiwv", "-tbn", "firrtl"), () => new FixedIterativeCordic(params)) {
       c => new FixedCordicTester(c, trials)
     }
+  }
+}
+
+class FixedCordicSpec extends FlatSpec with Matchers {
+  behavior of "FixedIterativeCordic"
+
+  val params = FixedCordicParams(
+    xyWidth = 32,
+    xyBPWidth = 16,
+    zWidth = 32,
+    zBPWidth = 20,
+    correctGain = true,
+    nStages = 32,
+    stagesPerCycle = 1,
+    calAtan2 = false,
+    dividing = true,
+  )
+
+  def angleMap(phi: Double): Double = {
+    if (phi > math.Pi / 2)
+      phi - math.Pi
+    else
+      if (phi < -math.Pi / 2)
+        phi + math.Pi
+      else
+        phi
+  }
+
+  val baseTrial = ABC(xin = 0.0, yin = 0.0, zin = 0.0, vectoring = true)
+  var yin = Seq(1, 2, 4, 8, 16, 31, 57)
+  var angles = Seq(-3.0, -2.0, -1.0, -0.0, -1.0, 2.0, 3.0)
+
+  val dividingTrials =
+    // check dividing
+    yin.map { y =>
+      baseTrial.copy(xin = -29, yin = y, zout = Some(y / -29.0))
+    }
+
+  val atan2Trials =
+    // check atan2
+    angles.map { phi =>
+      baseTrial.copy(xin = 15 * math.cos(phi), yin = 15 * math.sin(phi), zout = Some(phi))
+    }
+
+  val atanTrials =
+    // check atan
+    angles.map { phi =>
+      baseTrial.copy(xin = 15 * math.cos(phi), yin = 15 * math.sin(phi), zout = Some(angleMap(phi)))
+    }
+
+  it should "dividing with stagesPerCycle=1" in {
+    FixedCordicTester(params.copy(calAtan2=false, dividing=true), dividingTrials) should be (true)
+  }
+
+  it should "atan2 with stagePerCycle=1" in {
+    FixedCordicTester(params.copy(calAtan2=true, dividing=false), atan2Trials) should be (true)
+  }
+
+  it should "atan with stagePerCycle=1" in {
+    FixedCordicTester(params.copy(calAtan2=false, dividing=false), atanTrials) should be (true)
   }
 }
